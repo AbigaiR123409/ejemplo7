2366832737015a39b8bbca4225cdeaf9321068a3
AbigaiR123409 <abigailrf.ti22@utsjr.edu.mx>
2025-03-09 21:36:06 -0600

    f

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterLink, RouterOutlet} from '@angular/router';
 import { LibroComponent } from './pages/libro/libro.component';
 import { ProductoComponent } from './pages/producto/producto.component';
 import { PeliculaComponent } from './pages/pelicula/pelicula.component';
+import { MenuComponent } from "./pages/menu/menu.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,],
+  imports: [RouterOutlet, MenuComponent],
   templateUrl: './app.component.html', 
   styleUrl: './app.component.css'
 })
